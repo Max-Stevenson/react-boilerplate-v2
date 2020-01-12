1a2669ac6b305ff18cafacff1b5ab85ad0002c0b
Max Stevenson <maxstevenson@msn.com>
2020-01-12 18:57:25 +0000

    Finalize boilerplate.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { login, logout } from './actions/auth';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
-import '../styles/styles.scss';
+import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
